a0780fe7dc29514da0fa1b00154e5a2977dd2436
Javier Aguilar <coyote176@msn.com>
2020-08-23 17:21:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -15,3 +15,17 @@ end
 
 
 
+class Project
+  attr_reader :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+
+end
